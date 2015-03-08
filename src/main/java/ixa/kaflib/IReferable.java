1df2343b1a388f108daa0ccd197b0b3790c3025e
aprosio <alessio@apnetwork.it>
2015-03-08 12:06:02 +0100

    TLinkReferable implementation IReferable

--- src/main/java/ixa/kaflib/IReferable.java
+++ src/main/java/ixa/kaflib/IReferable.java
@@ -1,10 +1,8 @@
 package ixa.kaflib;
 
 
-public abstract class IReferable implements Comparable<IReferable>
+public abstract class IReferable implements Comparable<IReferable>, TLinkReferable
 {
-    public abstract String getId();
-
 	@Override
 	public int compareTo(IReferable o) {
 		return this.getId().compareTo(o.getId());
