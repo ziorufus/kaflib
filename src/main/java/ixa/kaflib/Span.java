368d580bd1c7d48a4735c1d86d6b095fa129e614
aprosio <alessio@apnetwork.it>
2015-04-17 12:33:06 +0200

    Caro Francesco, ho modificato la classe `Span` della Kaflib, e ora prevede la possibilità di avere più teste.
    Prima era:
        private T head;
    
    Ora è:
        private Set<T> heads;
    
    Per mantenere la retro-compatibilità, ho fatto questo:
    - `public boolean hasHead()` restituisce `true` se c'è almeno una testa
    - `public T getHead()` restituisce una testa a casaccio (ho comunque deprecato il metodo, da togliere quando capiamo quanto sbattimento causa)
    - ho aggiunto `public Set<T> getHeads()`
    - `public boolean isHead(T target)` verifica se `target` appartiene al `Set` delle teste
    - `public void setHead(T head)` aggiunge `head` al `Set` delle teste (comportamento analogo per `public void addTarget(T target, boolean isHead)`)
    
    Spero sia tutto chiaro.

--- src/main/java/ixa/kaflib/Span.java
+++ src/main/java/ixa/kaflib/Span.java
@@ -1,8 +1,10 @@
 package ixa.kaflib;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.ArrayList;
 import java.io.Serializable;
+import java.util.Set;
 
 public class Span<T> implements Serializable {
 	@Override
@@ -13,94 +15,106 @@ public class Span<T> implements Serializable {
 	}
 
 	//private List<String> targets;
-    private List<T> targets;
-    private T head;
+	private List<T> targets;
+	private Set<T> heads;
 
-    Span() {
-	this.targets = new ArrayList<T>();
-	this.head = null;
-    }
-
-    Span(List<T> targets) {
-	this.targets = targets;
-	this.head = null;
-    }
-
-    Span(List<T> targets, T head) {
-	this.targets = targets;
-	this.head = head;
-    }
-
-    public boolean isEmpty() {
-	return (this.targets.size() <= 0);
-    }
-
-    public String getStr() {
-        StringBuilder builder = new StringBuilder();
-        for (Object term : targets) {
-            if (builder.length() != 0) {
-                builder.append(' ');
-            }
-            if (term instanceof Term) {
-                builder.append(((Term) term).getStr());
-            } else if (term instanceof WF) {
-                builder.append(((WF) term).getForm());
-            } else {
-                builder.append(term.toString());
-            }
-        }
-        return builder.toString();
-    }
-    
-    public List<T> getTargets() {
-	return this.targets;
-    }
-
-    public T getFirstTarget() {
-	return this.targets.get(0);
-    }
-
-    public boolean hasHead() {
-	return (this.head != null);
-    }
-
-    public T getHead() {
-	return this.head;
-    }
-
-    public boolean isHead(T target) {
-	return (target == this.head);
-    }
-
-    public void setHead(T head) {
-	this.head = head;
-    }
-
-    public void addTarget(T target) {
-	this.targets.add(target);
-    }
-
-    public void addTarget(T target, boolean isHead) {
-	this.targets.add(target);
-	if (isHead) {
-	    this.head = target;
+	Span() {
+		this.targets = new ArrayList<T>();
+		this.heads = new HashSet<>();
 	}
-    }
 
-    public void addTargets(List<T> targets) {
-	this.targets.addAll(targets);
-    }
-
-    public boolean hasTarget(T target) {
-	for (T t : targets) {
-	    if (t == target) {
-		return true;
-	    }
+	Span(List<T> targets) {
+		this.targets = targets;
+		this.heads = new HashSet<>();
 	}
-	return false;
-    }
 
-    public int size() {
-	return this.targets.size();
-    }
+	Span(List<T> targets, T head) {
+		this.targets = targets;
+		this.heads = new HashSet<>();
+		heads.add(head);
+	}
+
+	public boolean isEmpty() {
+		return (this.targets.size() <= 0);
+	}
+
+	public String getStr() {
+		StringBuilder builder = new StringBuilder();
+		for (Object term : targets) {
+			if (builder.length() != 0) {
+				builder.append(' ');
+			}
+			if (term instanceof Term) {
+				builder.append(((Term) term).getStr());
+			}
+			else if (term instanceof WF) {
+				builder.append(((WF) term).getForm());
+			}
+			else {
+				builder.append(term.toString());
+			}
+		}
+		return builder.toString();
+	}
+
+	public List<T> getTargets() {
+		return this.targets;
+	}
+
+	public T getFirstTarget() {
+		return this.targets.get(0);
+	}
+
+	public boolean hasHead() {
+		return (this.heads.size() > 0);
+	}
+
+	@Deprecated
+	public T getHead() {
+		for (T term : this.heads) {
+			return term;
+		}
+
+		return null;
+	}
+
+	public Set<T> getHeads() {
+		return heads;
+	}
+
+	public boolean isHead(T target) {
+		return (this.heads.contains(target));
+	}
+
+	public void setHead(T head) {
+		this.heads.add(head)
+	}
+
+	public void addTarget(T target) {
+		this.targets.add(target);
+	}
+
+	public void addTarget(T target, boolean isHead) {
+		this.targets.add(target);
+		if (isHead) {
+			this.heads.add(target);
+		}
+	}
+
+	public void addTargets(List<T> targets) {
+		this.targets.addAll(targets);
+	}
+
+	public boolean hasTarget(T target) {
+		for (T t : targets) {
+			if (t == target) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public int size() {
+		return this.targets.size();
+	}
 }
